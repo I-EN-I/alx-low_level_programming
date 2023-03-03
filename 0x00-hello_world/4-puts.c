9feb275a74a77e437c00183a12d1469ff87fedea
I-EN-I <louethegreat@protonmail.com>
2023-03-03 12:37:25 -0800

    GDD35SV

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#inlcude <stdio.h>
-    
-    /*
-    * I have main
-    * I have the braces and
-    * I have the puts function
-    */
+#include <stdio.h>
 
-    int main(void)
+/*
+* I have main
+* I have the braces and
+* I have the puts function
+*/
+
+int main(void)
     {
         puts("\"Programming is like building a multilingual puzzle\n";
         return 0;
