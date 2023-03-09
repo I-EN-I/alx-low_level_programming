8d534f80cdafb3e28c7df0e8c66b3e0e7d5602c5
I-EN-I <louethegreat@protonmail.com>
2023-03-09 13:23:51 -0800

    dgre

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-    
-    /*
-    * I have main
-    * I have the braces and
-    * I have the printf function
-    */
 
-     int main(void)
-     {
-       printf("The size of int is: %d.\n", sizeof(int));
-       printf("The size of char is: %d.\n", sizeof(char));
-       return 0;
-     }
+/*
+* main - function
+* printf: printing out sizes
+* return: returning 0
+*/
+
+int main(void)
+{
+	printf("The size of int is: %d.\n", sizeof(int));
+	printf("The size of char is: %d.\n", sizeof(char));
+	return 0;
+}
