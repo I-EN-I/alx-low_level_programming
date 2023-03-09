5392f8908ffb53fe5bfc1af3b4d604e9c8679ae4
I-EN-I <louethegreat@protonmail.com>
2023-03-09 12:39:16 -0800

    shdd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-    
-    /*
-    * I have main
-    * I have the braces and
-    * I have the printf function
-    */
-    
-     int main(void)
-     {
-       printf("with proper grammar, but the outcome is a piece of art,\n");
-       return 0;
-     }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main()
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
